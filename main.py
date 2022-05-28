e656641def219cb0cac3121ed9a35bc911b1eaa6
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2022-05-28 14:38:58 +0530

    now loads custom file and search multiple words.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 #word=["COMPILER","SERVER","IMPORT","CPU","PSEUDOCODE",'PROMPT']
-f=open("text.txt","r")
-lines=f.read().split("\n")
+import sys
 def search(word,count=0):
-    for i,line in enumerate(lines):
-        if word in line:
-            count+=1
-    print("{} has repeated {} times".format(word,count))
-    count=0
-search("COMPILER")
+    for j in word:
+        for i,line in enumerate(lines):
+            if j in line:
+                count+=1
+        print("{} has repeated {} times".format(j,count))
+        count=0
+file_name=input("Enter the path for text file:")
+try:
+    f=open(file_name,"r")
+except FileNotFoundError:
+    print("File dosent exists")
+    sys.exit()
+lines=f.read().split("\n")
+print("File loaded successfully")
+word=input("Enter the word to be searched(case sensitive, for multiple add , after each word):").split(",")
+search(word)
 f.close()
