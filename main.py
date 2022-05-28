9b39ddccb7730be11281b78b72b46f920037ec09
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2022-05-28 14:30:07 +0530

    Converted the program into function...

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-count=0
-word=["COMPILER","SERVER","IMPORT","CPU","PSEUDOCODE",'PROMPT']
-with open("test.txt","r") as f:
-    lines=f.read().split("\n")
-    for j in word:
-        for i,line in enumerate(lines):
-            if j in line:
-                count+=1
-        print("{} has repeated {} times".format(j,count))
-        count=0
+#word=["COMPILER","SERVER","IMPORT","CPU","PSEUDOCODE",'PROMPT']
+f=open("text.txt","r")
+lines=f.read().split("\n")
+def search(word,count=0):
+    for i,line in enumerate(lines):
+        if word in line:
+            count+=1
+    print("{} has repeated {} times".format(word,count))
+    count=0
+search("COMPILER")
+f.close()
